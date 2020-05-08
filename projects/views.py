3346b5488d3946dca77ad59bb4bd0c62d1a39689
James Coleman <jcolem22@msudenver.edu>
2020-05-08 12:14:35 -0600

    set up html template.

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def project_list(request):
-	return HttpResponse("<h1>First Django Web Page</h1>")
+	return render(request, 'projects/index.html')

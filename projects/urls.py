a0b3f6992f3ff0c5635168fdac58982374f5c9ed
James Coleman <jcolem22@msudenver.edu>
2020-05-10 12:12:30 -0600

    Linked the individual page to the multiple projects page with buttons.

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from projects import views
 
+app_name = 'projects'
 
 urlpatterns = [
-	path('', views.all_projects),
-    path('<int:primary_key>', views.project_page)
+	path('', views.all_projects, name='all_projects'),
+    path('<int:primary_key>', views.project_page, name='read_more')
 ]

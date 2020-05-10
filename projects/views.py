05acedcee34eeb088e3ab4c2aa58078d1216c85e
James Coleman <jcolem22@msudenver.edu>
2020-05-10 12:33:51 -0600

    Combined a bunch of html code.

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from projects.models import Project
 
 
-def project_list(request):
-	return render(request, 'projects/index.html')
-
 
 def all_projects(request):
 	# Get projects from db
